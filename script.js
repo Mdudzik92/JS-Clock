86933efc842ea15ce47c8d62a15ee8ba5c35be9e
Matt Dudzik <mdudzik92@gmail.com>
2021-07-28 22:57:57 -0500

    Transforming secondHand div from the DOM to rotate by seconds and degrees

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+const secondHand = document.querySelector(".second-hand");
+
 function setDate() {
 	const now = new Date();
 	const seconds = now.getSeconds();
-	console.log(seconds);
+	// seconds / 60 gives the percentage so that 100% = 360 degrees.
+	// Then multiplying it by 360 gives the full circle
+	const secondsDegrees = (seconds / 60) * 360 + 90;
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
