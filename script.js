9403f14ef0b64a3329f5e3eae23da8f351e23878
Matt Dudzik <mdudzik92@gmail.com>
2021-07-28 23:17:50 -0500

    Getting minutes, rotating them by degrees

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const secondHand = document.querySelector(".second-hand");
+const minsHand = document.querySelector(".min-hand");
 
 function setDate() {
 	const now = new Date();
 	const seconds = now.getSeconds();
-	// seconds / 60 gives the percentage so that 100% = 360 degrees.
-	// Then multiplying it by 360 gives the full circle
+	// Seconds / 60 gives the percentage so that 100% = 360 degrees.
+	// Adding 90 offsets the default 90degrees transform in the CSS
 	const secondsDegrees = (seconds / 60) * 360 + 90;
+	// Applying transform style to second-hand div using ES6 template literal
 	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+	const mins = now.getMinutes();
+	const minsDegrees = (mins / 60) * 360 + 90;
+	minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
